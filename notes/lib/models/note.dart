3b74fa7c4c739133649e0aa9001bdb5d7e6bd5f0
Andreyas3667 <andreyasming@gmail.com>
2024-05-23 22:28:26 +0700

    Update
    Update 4

--- notes/lib/models/note.dart
+++ notes/lib/models/note.dart
@@ -4,7 +4,9 @@ class Note {
   String? id;
   final String title;
   final String description;
-  String? image_url;
+  String? imageUrl;
+  String? lat;
+  String? lng;
   Timestamp? createdAt;
   Timestamp? updatedAt;
 
@@ -12,7 +14,9 @@ class Note {
     this.id,
     required this.title,
     required this.description,
-    this.image_url,
+    this.imageUrl,
+    this.lat,
+    this.lng,
     this.createdAt,
     this.updatedAt,
   });
@@ -23,7 +27,9 @@ class Note {
       id: doc.id,
       title: data['title'],
       description: data['description'],
-      image_url: data['image_url'],
+      imageUrl: data['image_url'],
+      lat: data['lat'],
+      lng: data['lng'],
       createdAt: data['created_at'] as Timestamp,
       updatedAt: data['updated_at'] as Timestamp,
     );
@@ -33,7 +39,9 @@ class Note {
     return {
       'title': title,
       'description': description,
-      'image_url': image_url,
+      'image_url': imageUrl,
+      'lat': lat,
+      'lng': lng,
       'created_at': createdAt,
       'updated_at': updatedAt,
     };
